d2bf1bce8e838855c5c92e7f035d8ded9c3103de
Jorge Antonio <matroid@outlook.com>
2025-03-14 15:12:20 +0000

    add attestation for all devices

--- remote-attestation/src/tests.rs
+++ remote-attestation/src/tests.rs
@@ -35,7 +35,7 @@ fn generate_new_evidence() -> (Vec<DeviceEvidence>, String) {
             evidence: STANDARD.encode(attestation_report.attestation_report),
         });
     }
-    (evidence_vec, hex::encode(nonce))
+    (evidence_vec, "931d8dd0add203ac3d8b4fbde75e115278eefcdceac5b87671a748f32364dfcb".to_string())
 }
 
 #[tokio::test]
