0ce718010e851c24f660a1a6d26368b1fd8fcb85
Jorge Antonio <matroid@outlook.com>
2025-03-14 15:08:50 +0000

    add more tests

--- remote-attestation/src/tests.rs
+++ remote-attestation/src/tests.rs
@@ -1,8 +1,12 @@
 use std::{fs, path::Path};
 
+use base64::{engine::general_purpose::STANDARD, Engine};
+use nvml_wrapper::Nvml;
+use rand::Rng;
+
 use crate::{attest_remote, DeviceEvidence};
 
-fn generate_new_device_evidence() -> (Vec<DeviceEvidence>, String) {
+fn read_working_evidence() -> (Vec<DeviceEvidence>, String) {
     let file = Path::new("./evidence/evidence.json");
     let evidence = fs::read_to_string(file).expect("Failed to read evidence file");
     let evidence: Vec<DeviceEvidence> =
@@ -13,9 +17,40 @@ fn generate_new_device_evidence() -> (Vec<DeviceEvidence>, String) {
     )
 }
 
+fn generate_new_evidence() -> (Vec<DeviceEvidence>, String) {
+    let nvml = Nvml::init().expect("Failed to initialize NVML");
+    let device = nvml.device_by_index(0).expect("Failed to get device");
+    let nonce = rand::thread_rng().gen::<[u8; 32]>();
+    let attestation_report = device
+        .confidential_compute_gpu_attestation_report(nonce)
+        .expect("Failed to get report");
+    let certificate = device
+        .confidential_compute_gpu_certificate()
+        .expect("Failed to get certificate");
+    let evidence = vec![DeviceEvidence {
+        certificate: STANDARD.encode(certificate.attestation_cert_chain),
+        evidence: STANDARD.encode(attestation_report.attestation_report),
+    }];
+    (evidence, hex::encode(nonce))
+}
+
 #[tokio::test]
-async fn test_retrieve_attestation_evidence() {
-    let (evidence, nonce) = generate_new_device_evidence();
+async fn test_attest_working_evidence() {
+    let (evidence, nonce) = read_working_evidence();
+    match attest_remote(&evidence, &nonce, None, None, None).await {
+        Ok((attestation_passed, jwt)) => {
+            println!("Attestation passed: {}", attestation_passed);
+            println!("JWT: {}", jwt);
+        }
+        Err(e) => {
+            panic!("Failed to attest remote: {}", e);
+        }
+    }
+}
+
+#[tokio::test]
+async fn test_attest_new_evidence() {
+    let (evidence, nonce) = generate_new_evidence();
     match attest_remote(&evidence, &nonce, None, None, None).await {
         Ok((attestation_passed, jwt)) => {
             println!("Attestation passed: {}", attestation_passed);
