254ba215dce1fe1d56266fbc8c7e1f7bffd21ab4
Jorge Antonio <matroid@outlook.com>
2025-03-14 15:18:23 +0000

    debugging

--- remote-attestation/src/utils.rs
+++ remote-attestation/src/utils.rs
@@ -268,7 +268,7 @@ pub mod nras_token {
 
         // Decode the token with our custom claims structure
         let token_data = decode::<NvidiaAttestationClaims>(token, &decoding_key, &validation)?;
-        dbg!("token_data: {:?}", token_data);
+        dbg!("token_data: {:?}", token_dat.clone());
         Ok(token_data.claims)
     }
 }
