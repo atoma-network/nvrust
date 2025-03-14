dfdeb3330989511ddc21a5dddfff874adb7f193c
Jorge Antonio <matroid@outlook.com>
2025-03-14 15:04:02 +0000

    update code for testing

--- remote-attestation/src/tests.rs
+++ remote-attestation/src/tests.rs
@@ -1,22 +1,22 @@
 use std::{fs, path::Path};
 
-use rand::Rng;
-
 use crate::{attest_remote, DeviceEvidence};
 
-fn generate_new_device_evidence() -> (DeviceEvidence, String) {
+fn generate_new_device_evidence() -> (Vec<DeviceEvidence>, String) {
     let file = Path::new("./evidence/evidence.json");
     let evidence = fs::read_to_string(file).expect("Failed to read evidence file");
-    let evidence: DeviceEvidence =
+    let evidence: Vec<DeviceEvidence> =
         serde_json::from_str(&evidence).expect("Failed to parse evidence");
-    let nonce = rand::thread_rng().gen::<[u8; 32]>();
-    (evidence, hex::encode(nonce))
+    (
+        evidence,
+        "931d8dd0add203ac3d8b4fbde75e115278eefcdceac5b87671a748f32364dfcb".to_string(),
+    )
 }
 
 #[tokio::test]
 async fn test_retrieve_attestation_evidence() {
     let (evidence, nonce) = generate_new_device_evidence();
-    match attest_remote(&[evidence], &nonce, None, None, None).await {
+    match attest_remote(&evidence, &nonce, None, None, None).await {
         Ok((attestation_passed, jwt)) => {
             println!("Attestation passed: {}", attestation_passed);
             println!("JWT: {}", jwt);
